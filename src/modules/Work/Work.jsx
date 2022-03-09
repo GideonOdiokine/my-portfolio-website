9bf088b737981180be8323d717b2305bb134ca48
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-09 23:12:36 +0100

    working on the work component

--- src/modules/Work/Work.jsx
+++ src/modules/Work/Work.jsx
@@ -20,6 +20,32 @@ const Work = () => {
               amet sint. Velit officia consequat duis enim velit mollit.
               Exercitation veniam consequat sunt nostrud amet.
             </p>
+            <a href="https://creditwaveng.com" rel="noreferrer">
+              <span>view</span>
+            </a>
+            <a href="https://creditwaveng.com" rel="noreferrer">
+              <i class="lni lni-github-original"></i>
+            </a>
+          </div>
+        </div>
+        <div className="work__container">
+          <div className="img">
+            <img src={rec} alt="work_pic" />
+          </div>
+          <div className="content">
+            <h3>Designing Creditwave</h3>
+            <strong>Dashboard</strong>
+            <p>
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+            <a href="https://creditwaveng.com" rel="noreferrer">
+              <span>view</span>
+            </a>
+            <a href="https://creditwaveng.com" rel="noreferrer">
+              <i class="lni lni-github-original"></i>
+            </a>
           </div>
         </div>
       </div>
