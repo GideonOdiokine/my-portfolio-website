c7ea7d359553a0ca5e9f9d3512d2adbc32a4ce39
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-09 07:13:49 +0100

    added download resume

--- src/modules/Showcase/Showcase.jsx
+++ src/modules/Showcase/Showcase.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import picture from "../../assets/img/gidy.png";
 import "./Showcase.scss";
 
@@ -16,7 +15,29 @@ const Showcase = () => {
               amet sint. <br /> Velit officia consequat duis enim velit mollit.
               Exercitation veniam consequat <br /> sunt nostrud amet.
             </p>
-            <Link to="/">Download Resume</Link>
+            <a
+              rel="noreferrer"
+              href="https://docs.google.com/document/d/e/2PACX-1vTFGDJxSlY7rhzxn-4q3NUqrxVuyEi4nqh-NszE-rHg3BqPqpHDBEQ1IsouiwcN1h_jSIHXYALnmZC_/pub"
+              target="_blank"
+              data-text=" Résumé."
+            >
+              <span>D</span>
+              <span>o</span>
+              <span>w</span>
+              <span>n</span>
+              <span>l</span>
+              <span>o</span>
+              <span>a</span>
+              <span>d</span>
+              <span> </span>
+              <span>R</span>
+              <span>é</span>
+              <span>s</span>
+              <span>u</span>
+              <span>m</span>
+              <span>é</span>
+              <span>.</span>
+            </a>
           </div>
           <div className="showcase__right">
             <img src={picture} alt="profile" />
