854f097d854e8b41808d8b0dc68f13bd3aa80860
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-10 00:02:00 +0100

    finished working on footer

--- src/modules/HomeComponent/Showcase/Showcase.jsx
+++ src/modules/HomeComponent/Showcase/Showcase.jsx
@@ -9,11 +9,11 @@ const Showcase = () => {
         <div className="flex">
           <div className="showcase__left">
             <h2>Hi, I am Gideon,</h2>
-            <h2>Creative Frontend Developer.</h2>
+            <h2>Creative Frontend Engineer.</h2>
             <p>
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. <br /> Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat <br /> sunt nostrud amet.
+              I am a Frontend developer with a passion for web technologies. I
+              write robust, clean,
+              <br /> re-usable and maintainable code.
             </p>
             <a
               rel="noreferrer"
